93b6f1c56fbfda0e00238f4f84dc12a96fdf448a
Nande Ayô <104651656+FernandeAyo@users.noreply.github.com>
2022-08-04 17:25:39 -0300

    Add files via upload

---
+++ calculadora_simples/funcoes.js
@@ -0,0 +1,25 @@
+
+function inserir(num) {
+    var visor = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = visor + num;
+}
+
+function limpar() {
+    document.getElementById('visor').innerHTML = " ";
+}
+
+function apagar() {
+    var visor = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = visor.substring(0, visor.length-1);
+}
+
+function calcular() {
+    var visor = document.getElementById('visor').innerHTML;
+    
+    //Não sei porque não funciona para deixar o visor vazio
+    if(visor == " "){
+        document.getElementById('visor').innerHTML = " ";
+    } else {
+        document.getElementById('visor').innerHTML = eval(visor);
+    }
+}
